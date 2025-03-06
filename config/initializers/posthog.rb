2565b85a12fedb6428be31ed053041d410c09b28
Odair Santiago <odairsantiago@Odairs-MacBook-Air.local>
2025-03-06 15:47:52 -0300

    add personal api key to posthog

--- config/initializers/posthog.rb
+++ config/initializers/posthog.rb
@@ -1,5 +1,6 @@
 Rails.application.config.posthog = PostHog::Client.new({
   api_key: ENV["POSTHOG_API_KEY"],
+  personal_api_key: ENV["POSTHOG_PERSONAL_API_KEY"],
   host: ENV["POSTHOG_HOST"],
   on_error: Proc.new { |status, msg| print msg }
 })
